bff4a0d2ed664c0d897f3842fae304bf6141bfa3
nhatpham1997 <nhatpham1997ht@gmail.com>
2021-09-26 17:42:25 +0700

    add footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import SmallCard from "../components/SmallCard";
 import MediumCard from '../components/MediumCard'
 import LargeCard from "../components/LargeCard";
+import Footer from "../components/Footer";
 
 export default function Home({ exploreData, cardsData }) {
     return (
@@ -33,6 +34,7 @@ export default function Home({ exploreData, cardsData }) {
                 </section>
                 <LargeCard img="https://links.papareact.com/4cj" title="The Greatest Outdoors" description="Wishlists curated by Airbnb" buttonText="Get Inspired" />
             </main>
+            <Footer />
         </div>
     );
 }
