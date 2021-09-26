212afe5da02ebeb21dcf4070d527c18502a7156f
nhatpham1997 <nhatpham1997ht@gmail.com>
2021-09-27 01:59:06 +0700

    add Mapbox

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
     images: {
         domains: ["links.papareact.com"],
     },
+    env: {
+        mapbox_key: 'pk.eyJ1IjoibmhhdHBoYW0iLCJhIjoiY2t1MWlxeXZjMTRyeDJvczg1d2ozdm4xNSJ9.DhPalT3thri7PqyLxi-Ktw'
+    }
 };
